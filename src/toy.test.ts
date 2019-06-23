ec472b53ee23685887178b30b08cddb19e7328b1
Chris Shepherd <thesheps@gmail.com>
2019-06-23 15:07:38 +0100

    feat: Initial check-in, simple passing TypeScript test

---
+++ src/toy.test.ts
@@ -0,0 +1,11 @@
+describe("Toy", () => {
+  it("looks like TypeScript is correctly configured", () => {
+    class MyLovelyClass {
+      thing: string;
+
+      constructor(thing: string) {
+        this.thing = thing;
+      }
+    }
+  });
+});
